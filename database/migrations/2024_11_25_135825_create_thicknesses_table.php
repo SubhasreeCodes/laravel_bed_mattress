b1d0e8869e27545dff2bf0a4b27864129c299df7
SubhasreeCodes <subhasreeps2001@gmail.com>
2024-11-25 19:38:24 +0530

    Create Thickness Models and Migrations
    command:
    php artisan make:model thickness -m

---
+++ database/migrations/2024_11_25_135825_create_thicknesses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thicknesses', function (Blueprint $table) {
+            $table->id();
+            $table->string('value_in_inches',5,2);  // e.g., 4,6,8,10
+            $table->string('value_in_feet',5,2);    // e.g., 0.33, 0.5,0.67, 0.83
+            $table->string('value_in_centimeters',5,2); // e.g., 10.16, 15.24, 20.32, 25.4
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thicknesses');
+    }
+};
